1d387076b75d67c55981dfa17b1b3f2316378b32
sebastiansalinaz <jspaz58@misena.edu.co>
2023-06-12 14:45:12 -0500

    pagos

---
+++ app/Models/Pagos.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class pagos extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'value_pay',
+        'pay_day'
+    ];
+}
