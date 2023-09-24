15d988ba7453f6f374db6e7d1c690b656083b556
[JohanChate] <[jachate7@misena.edu.co]>
2023-09-24 14:23:47 -0500

    reparacion card de rectas

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->string('external_id')->nullable();
             $table->string('external_auth')->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password')->nullable()->change();
+            $table->string('password')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
